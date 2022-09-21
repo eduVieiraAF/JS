2c79195257808c388f49e75047a527fd8fa83751
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-21 11:22:34 -0300

    Simple Math built-in methods

---
+++ JS tutorial/Example06.js
@@ -0,0 +1,17 @@
+let x = 3.14159
+let y = 5
+let z = 9
+let max
+let min
+
+// x = Math.round(x)
+// x = Math.floor(x)
+// x = Math.ceil(x)
+// x = Math.pow(x, 100)
+// x = Math.sqrt(x)
+// x = Math.abs(x)
+// max = Math.max(x, y, z)
+// min = Math.min(x, y, z)
+x = Math.PI
+
+console.log(x)
