11a4477d7f63c53ab0a0487ad79194a4f85c59e2
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-21 10:56:52 -0300

    Const

---
+++ JS tutorial/Example05.js
@@ -0,0 +1,11 @@
+
+const PI = 3.114159
+let radius
+let circumference
+
+radius = window.prompt("Enter the radius")
+radius = Number(radius)
+
+circumference = 2 * PI * radius
+
+console.log(circumference, "is the circumference")
