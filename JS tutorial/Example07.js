e7b1be3f96ebc88eea521f5d2ea9fc5120ace369
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-21 12:05:09 -0300

    Calculate hypotenuse Taking input from html

---
+++ JS tutorial/Example07.js
@@ -0,0 +1,15 @@
+let a
+let b
+let c 
+
+document.getElementById("button").onclick = function() {
+    a = document.getElementById("sideA").value
+    a = Number(a)
+
+    b = document.getElementById("sideB").value
+    b = Number(b)
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+    console.log("Side C →", c)
+    document.getElementById("sideC").innerHTML = "Side C → " + c
+}
