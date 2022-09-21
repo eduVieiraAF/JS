116018f9e06aa3042dacf7645a73e429ab472c68
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-21 14:28:18 -0300

    Adding some complexity to the very basic JS code

--- JS tutorial/Example07.js
+++ JS tutorial/Example07.js
@@ -3,13 +3,19 @@ let b
 let c 
 
 document.getElementById("button").onclick = function() {
+
     a = document.getElementById("sideA").value
     a = Number(a)
 
     b = document.getElementById("sideB").value
     b = Number(b)
 
-    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
-    console.log(`Side C → ${c}`)
-    document.getElementById("sideC").innerHTML = `Side C → ${c}.`
+    if (a != "" || b != "" || a != 0 || b!=0) {
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+        console.log(`Side C → ${c}`)
+        document.getElementById("sideC").innerHTML = `Side C → ${c}.`
+    } else {
+        console.log("ZERO, NaN or null")
+        window.alert("Enter the sides")
+    }
 }
