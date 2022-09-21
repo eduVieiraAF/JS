8c7a9b20a5ad6a9306978b27e5c269dd99702062
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-21 10:47:42 -0300

    Types and type conversion

---
+++ JS tutorial/Example04.js
@@ -0,0 +1,25 @@
+//let age = window.prompt("How old are you?")
+
+/*
+console.log(typeof age)
+
+age = Number(age)
+age += 1
+
+console.log("Happy birthday", age, "years old.")
+console.log(typeof age) */
+
+let x
+let y 
+let z
+let nah
+
+x = Number("3.14")
+y = String(3.14)
+z = Boolean("lego") // False when ""
+nah = Number("fudge") // NaN (not a number)
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+console.log(nah, typeof nah)
