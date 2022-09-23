828ee3b4674bb16a4a895e18384f085f1ec9ba57
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-23 13:43:33 -0300

    Simple object

---
+++ JS tutorial/Example10.js
@@ -0,0 +1,5 @@
+const car = {make:"Ford", model:"Mustang", color:"Fiery red"}
+
+document.getElementById("lbl_make").innerHTML = car.make
+document.getElementById("lbl_model").innerHTML = car.model
+document.getElementById("lbl_color").innerHTML = car.color
