f0551a031c50171450586934b9c003e9991dd245
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-25 12:34:32 -0300

    Slice

--- JS tutorial/Example15.js
+++ JS tutorial/Example15.js
@@ -1,5 +1,9 @@
 let username = "    eduVieiraAF "
 let mblNum = "11-9-7398-3112"
+let usernameSliced1 = username.trim().slice(3)
+let usernameSliced2 = username.trim().slice(0, 3)
+let usernameSliced3 = username.slice(username.indexOf("u") + 1)
+let usernameSliced4 = username.slice(4, username.indexOf("u") + 1)
 
 console.log(username)
 console.log(username.length, mblNum.length)
@@ -9,6 +13,8 @@ console.log(username.lastIndexOf("e"))
 console.log(username.trim())
 console.log(username.toUpperCase().trim())
 console.log(username.toLocaleLowerCase().trim())
-console.log("__________________________________________")
-console.log("")
 console.log(mblNum.replaceAll("-", "."))
+console.log(usernameSliced1)
+console.log(usernameSliced2)
+console.log(usernameSliced3)
+console.log(usernameSliced4)
