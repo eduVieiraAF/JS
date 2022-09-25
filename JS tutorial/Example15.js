d3bccbb051474ebca45cbf80a23701b63598e733
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-25 12:25:59 -0300

    String Methods

---
+++ JS tutorial/Example15.js
@@ -0,0 +1,14 @@
+let username = "    eduVieiraAF "
+let mblNum = "11-9-7398-3112"
+
+console.log(username)
+console.log(username.length, mblNum.length)
+console.log(username.charAt(4))
+console.log(username.indexOf("e"))
+console.log(username.lastIndexOf("e"))
+console.log(username.trim())
+console.log(username.toUpperCase().trim())
+console.log(username.toLocaleLowerCase().trim())
+console.log("__________________________________________")
+console.log("")
+console.log(mblNum.replaceAll("-", "."))
