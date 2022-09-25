0472309c60fc05989a702c3f232f4fe4f23f1fbe
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-25 11:25:45 -0300

    Adding recursion

--- JS tutorial/Example13.js
+++ JS tutorial/Example13.js
@@ -10,7 +10,7 @@ for (let i = 1; i<= n; i++) {
     console.log(`hello, World: ${i}`)
 }
 
-console.log("-----------------------------------------")
+console.log("\n-----------------------------------------")
 
 /*
 while (condition) {
@@ -23,4 +23,23 @@ let start = 1, x = prompt("Enter a number");
 while (start <= x) {
     console.log(`hello, World: ${start}`)
     start += 1
+}
+
+console.log("\n-----------------------------------------")
+
+function factorial(x) {
+    if (x ===0) {
+        return 1
+    } else {
+        return x * factorial(x - 1)
+    }
+}
+
+let num = prompt("Enter a number")
+
+if (num > 0) {
+    let result = factorial(num)
+    console.log(`The factorial of ${num} is ${result}.`)
+} else {
+    console.log("Nope")
 }
