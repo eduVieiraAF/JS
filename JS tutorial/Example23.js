30da42120914f7d2f00ba82c7d3a20f368ce57b7
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-29 09:44:56 -0300

    from if to switch

--- JS tutorial/Example23.js
+++ JS tutorial/Example23.js
@@ -4,13 +4,25 @@ function myFunction() {
     let x = document.getElementById("numInput").value
 
     try {
-        if (x == "") throw "empty"
-        if (isNaN(x)) throw "Not a number"
+        switch (true) {
+            case x == "":
+                throw "empty"
+            
+            case isNaN(x):
+                throw "Not a number"
+        }
         
         x = Number(x)
-        if (x == 25) throw "right in the middle"
-        if (x < 25) throw "too low"
-        if (x > 25) throw "too high"
+        switch(true) {
+            case x == 25:
+                throw  "right in the middle"
+            
+            case x > 25: 
+                throw "too high"
+            
+            case x < 25:
+                throw "too low"
+        }
     } catch(err) {
         message.innerHTML = "Input is " + err
     }
