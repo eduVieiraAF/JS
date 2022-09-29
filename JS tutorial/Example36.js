cf22dd03d1d53bb81a7bb256edbe4fdec9336461
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-29 16:50:55 -0300

    Import / export

---
+++ JS tutorial/Example36.js
@@ -0,0 +1,10 @@
+const message = 'hello world';
+
+const number = 10;
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+// exporting variables and function
+export { message, number, multiplyNumbers };
---
+++ JS tutorial/Example36b.js
@@ -0,0 +1,9 @@
+// import the variables and function 
+import { message, number, multiplyNumbers } from './Example36';
+
+console.log(message); // hello world
+
+console.log(number); // 10
+
+console.log(multiplyNumbers(3, 4)); // 12
+console.log(multiplyNumbers(5, 8)); // 40
