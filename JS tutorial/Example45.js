2b41a0d05ccbcf0a014cfaa76369537cb8f1ae49
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-06 18:55:38 -0300

    the rest parameter

---
+++ JS tutorial/Example45.js
@@ -0,0 +1,32 @@
+
+//* The rest parameter  '...'
+/*
+    represents an indefinite number of arguments
+    (packs arguments into an array)
+*/
+
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+let e = 5
+
+
+console.log(sum(a, b))
+
+/* could not accept more than 2 arguments this way
+function sum(a, b) {
+    return a + b
+}
+*/
+
+function sum(...numbers) {
+    let total = 0
+
+    for (let number of numbers) {
+        total += number
+    }
+
+    return total
+}
+
