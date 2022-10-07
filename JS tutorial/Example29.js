693fc31ee1d67f07a4cda134a1c3db37b4c37645
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-07 09:16:24 -0300

    more on callbacks

--- JS tutorial/Example29.js
+++ JS tutorial/Example29.js
@@ -1,3 +1,14 @@
+//* Simple example before Async
+
+/*
+    A callback is a function that is passed as an argument to another function
+
+    → it ensures that a function is not going to run before a task is completed.
+    → it helps to develop asynchronous (when a function has to wait for another function) code
+        that helps us avoid errors and potential problems, such as waiting for a file to load.
+        
+*/
+
 function myDisplay(something) {
     document.getElementById("demo").innerHTML = something
 }
@@ -9,4 +20,20 @@ function myCalculator(num1, num2, myCallback) {
 
 myCalculator(5, 15, myDisplay)
 
-//* Simple example before Async
+
+sum(7, 5, dispalayConsole)
+sum(23, 12, displayDOM)
+
+function sum(x, y, aCallback) {
+    let result = x + y
+
+    aCallback(result)
+}
+
+function dispalayConsole(output) {
+    console.log(output)
+}
+
+function displayDOM(output) {
+    document.getElementById("total").innerHTML = output
+}
