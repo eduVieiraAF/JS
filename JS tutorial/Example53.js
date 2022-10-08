0df417d555fd6a43c5b6c39514a7ac0f79f26587
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-08 16:43:04 -0300

    History

---
+++ JS tutorial/Example53.js
@@ -0,0 +1,14 @@
+
+function goBack() {
+    window.history.back()
+    // window.history.go(-2) back 2 pages
+}
+
+/*
+length	    Returns the number of URLs in the history list
+
+back()	    Loads the previous URL in the history list
+forward()	Loads the next URL in the history list
+go()	    Loads a specific URL from the history list
+*/
+
