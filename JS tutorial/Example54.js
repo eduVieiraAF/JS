cd6e7cc2ecc1903a793713105ff400c74e66b4d8
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-09 10:57:21 -0300

    JS navigator

---
+++ JS tutorial/Example54.js
@@ -0,0 +1,18 @@
+
+document.getElementById("demo").innerHTML = "navigator.cookieEnable → " + navigator.cookieEnabled
+
+document.getElementById("demo2").innerHTML = "navigator.appCodeName → " + navigator.appCodeName
+
+document.getElementById("demo3").innerHTML = "navigator.product → " + navigator.product
+
+document.getElementById("demo4").innerHTML = navigator.appVersion
+
+document.getElementById("demo5").innerHTML = navigator.userAgent
+
+document.getElementById("demo6").innerHTML = navigator.platform
+
+document.getElementById("demo7").innerHTML = navigator.language
+
+document.getElementById("demo8").innerHTML = navigator.onLine;
+
+document.getElementById("demo9").innerHTML = navigator.javaEnabled()
