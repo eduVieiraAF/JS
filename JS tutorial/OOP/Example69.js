1d16039e16f72d2753cfea62f1a2b760fdc944cf
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-11 11:33:54 -0300

    Exception handling

---
+++ JS tutorial/OOP/Example69.js
@@ -0,0 +1,15 @@
+
+try{
+    let x = window.prompt("Enter a number")
+    x = Number(x)
+
+    if(isNaN(x)) throw "Not a number"
+    if(x == "") throw 'empty'
+
+    console.log(x)
+} catch(err) {
+    console.log(err)
+} finally {
+    // executes regards of conditions above
+    console.log("This will always execute")
+}
