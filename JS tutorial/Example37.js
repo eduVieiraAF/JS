582401a945e27a8a0e5a691ba73738889e500a12
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-13 16:25:44 -0300

    More on promise

--- JS tutorial/Example37.js
+++ JS tutorial/Example37.js
@@ -3,8 +3,16 @@
 "Consuming code" is code that must wait for the result
 
 A promise is a JavaScript object that links producing code and consuming code
+Promises encapsulate the result of an asynchronous operation so they let asynchronous methods
+    return values like synchronous methods.
+
+"I promise to return something in the future"
+
+?   STATES : 'pending' → 'fulfilled' → 'rejected'
 */
 
+
+//* Example #1
 function myDisplayer(some) {
     document.getElementById("demo").innerHTML = some
 }
@@ -23,3 +31,28 @@ myPromise.then(
     function(value) {myDisplayer(value)},
     function(error) {myDisplayer(error)}
 )
+
+//* Example #2
+
+//* Producing code
+function logger(value) {
+    console.log(value)
+}
+
+const promise = new Promise((resolve, reject) => {
+    let fileLoaded = true
+    
+    if (fileLoaded) { resolve("File loaded") } 
+    else { reject("File NOT found") }
+})
+
+//* consuming code
+promise.then(
+    (loaded) => {logger(loaded)},
+    (error) => {logger(error)}
+)
+ /* 
+ *  Alternatively
+ promise.then(value => console.log(value)).catch(error => console.log(error)) 
+ */
+
