fabb584eb2b597fd9eb0f7aa5ef3e47640851f43
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-20 07:54:57 -0300

    Changing CSS properties

---
+++ JS tutorial/DOM/Example81.js
@@ -0,0 +1,12 @@
+
+const title = document.getElementById("myTitle")
+
+title.style.fontFamily = "monospace"
+title.style.backgroundColor = "#343f6c"
+title.style.color = "white"
+title.style.padding = "8px"
+title.style.width = "250px"
+title.style.textAlign = "center"
+title.style.borderRadius = "8px"
+title.style.boxShadow = "2px 2px 2px gray, 4px 4px 6px gray"
+// title.style.display = "none"
