dbb2cec3fd275e2b293a92231c0a988f24fb4e75
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-21 15:50:12 -0300

    Part1
    → libs installed
    → connections ok
    → early resgistration logic in place

--- CRUD/src/Index.js
+++ CRUD/src/Index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+require('./controllers/authController')(app);
 
 app.listen(3000);
--- CRUD/src/controllers/authController.js
+++ CRUD/src/controllers/authController.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const User = require('../models/user');
+const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    try { 
-        const user = await User.create(req.body);
+const { email } = req.body;
 
-        return res.send(user);
+    try { 
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: "User already exists!" })
+
+        const user = await User.create(req.body)
+
+        user.password = undefined
+
+        return res.send({ user });
     } catch (err) {
-        console.error(err)
         return res.status(400).send({ error: 'Registration failed' })
     }
-})
+});
+
+module.exports = app => app.use('/auth', router);
