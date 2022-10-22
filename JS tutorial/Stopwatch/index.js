5fcb695e48b21b80685ecb00ccb7ca7c95944ede
Eduardo Vieira <edu_vieira@hotmail.com>
2022-10-22 17:25:03 -0300

    Simple stopwatch

---
+++ JS tutorial/Stopwatch/index.js
@@ -0,0 +1,60 @@
+const display = document.querySelector("#timeDisplay");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+startBtn.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 75);
+    }
+});
+
+pauseBtn.addEventListener("click", () => {
+    if (!paused) {
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    display.textContent = '00:00:00';
+});
+
+function updateTime() {
+    elapsedTime = Date.now() - startTime;
+
+    seconds = Math.floor((elapsedTime / 1000) % 60);
+    minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+    seconds = pad(seconds);
+    minutes = pad(minutes);
+    hours = pad(hours);
+
+    display.textContent = `${hours}:${minutes}:${seconds}`;
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
