74c199e70c180fb3e6691fc8b089beabacd6bf57
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-05 20:42:56 -0300

    mongodb

--- API_REST_example/src/server.ts
+++ API_REST_example/src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/apitest")
 
 app.use(express.json())
 app.get('/', (req, res) => {
