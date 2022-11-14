6b63c90eb19031058e63ba45dd05016f1d6e337f
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-14 15:06:48 -0300

    check & encrypt
    → BR to verify if user is already in DB;
    → Adding bcrypjs libs

--- API_REST_example/src/controller/UserController.ts
+++ API_REST_example/src/controller/UserController.ts
@@ -6,6 +6,13 @@ class UserController {
         const { name, email, password } = req.body;
         
         try {
+            const userExists = await User.findOne({ email })
+
+            if (userExists) return res.status(400).json({ 
+                error: "Ops..." ,
+                message: "User already exists!"
+            })
+
             const user = await User.create({
                 name,
                 email,
@@ -14,7 +21,7 @@ class UserController {
 
             return res.json(user);
         } catch (error) {
-            return res.status(500).send({
+            return res.status(500).json({
                 error: "Registration failed",
                 message: error
             })
